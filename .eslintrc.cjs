b37ee5462ea0c746fe23dd5f1238112d095dc100
Ferox <105577679+FeroxShark@users.noreply.github.com>
2025-06-04 08:42:03 +0100

    Add skeleton loaders, lightbox gallery, and snap scrolling

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  globals: {
+    React: 'readonly',
+    ReactDOM: 'readonly',
+    DOMPurify: 'readonly',
+  },
+  rules: {
+    'no-unused-vars': ['error', { args: 'none' }],
+  },
+};
